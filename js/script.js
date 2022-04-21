fe8bfbdd779246adfd7847fba48507be8549269e
Paparellav <vitantoniopaparella@gmail.com>
2022-04-21 15:18:25 +0200

    dovrebbe funzionare ma non funziona RIP

--- js/script.js
+++ js/script.js
@@ -2,25 +2,26 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-const userWord = isPalindrom();
+const userWord = prompt('Inserisci una parola');
 console.log(userWord);
+let myWord = checkPalindrom(userWord);
+console.log(myWord);
+let result;
+
+if (userWord === myWord) {
+    result = ('La parola è palindroma');
+} else {
+    result = ('La parola non è palindroma');
+}
+
+console.log(result);
 
 /**
  * Description // Funzione per determinare se la parola inserita dall'utente sia palindroma o meno
  * @param {string} userElement // La parola che andrà ad inserire l'utente nonché quella che verrà controllata nella function
  * @returns {string} // Risposta se la parola è palindroma o meno
  */
-function isPalindrom(userElement) {
 
-    userElement = prompt('Inserisci una parola');
-    let result;
-
-    if (userElement === ciao) {
-        result = true;
-        console.log(result);
-    } else {
-        result = false;
-        console.log(result);
-    }
-    return result;
+function checkPalindrom(userElement) {
+    return userElement === userElement.split('').reverse().join(''); 
 }
